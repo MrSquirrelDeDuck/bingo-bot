a48ee39fd8d047e792f2fcb87472a28a4e14d929
MrSquirrelDeDuck <72826048+MrSquirrelDeDuck@users.noreply.github.com>
2024-01-13 17:16:57 -0500

    Setup shutdown bot command

--- admin_cog.py
+++ admin_cog.py
@@ -4,11 +4,14 @@ import typing
 import traceback
 import copy
 import os
+import asyncio
+import random
 
 import sys
 
 import utility.text as u_text
 import utility.custom as u_custom
+import utility.checks as u_checks
 
 class Admin_cog(u_custom.CustomCog, name="Admin"):
     bot = None
@@ -173,6 +176,51 @@ class Admin_cog(u_custom.CustomCog, name="Admin"):
             reloaded_count += await self._smart_reload(f"{cog_name.lower()}_cog")
         
         return reloaded_count
+    
+    async def _await_confirmation(self, ctx: typing.Union[commands.Context, u_custom.CustomContext],
+            message = "Are you sure you would like to proceed? y/n.",
+            confirm: list[str] = ["y", "yes"], 
+            cancel: list[str] = ["n", "no"],
+            lower_response: bool = True
+        ) -> bool:
+        """Prompts a confirmation.
+
+        Args:
+            ctx (typing.Union[commands.Context, u_custom.CustomContext]): The context object.
+            message (str, optional): The message to prompt with. Defaults to "Are you sure you would like to proceed? y/n.".
+            confirm (list[str], optional): A list of strings that will be accepted. Defaults to ["y", "yes"].
+            cancel (list[str], optional): A list of strings that will cancel. Defaults to ["n", "no"].
+            lower_response (bool, optional): Whether to make the response lower case before checking it against the `confirm` and `cancel` lists. Defaults to True.
+
+        Returns:
+            bool: Whether the prompt was accepted.
+        """
+
+        def check(m: discord.Message):
+            return m.author.id == ctx.author.id and m.channel.id == ctx.channel.id 
+        
+        await ctx.reply(message)
+        try:
+            msg = await self.bot.wait_for('message', check = check, timeout = 60.0)
+        except asyncio.TimeoutError: 
+            # No message was sent by the person running the command in a minute, so just cancel it.
+            await ctx.reply(f"Timed out.")
+            return False
+        
+        response = msg.content
+
+        if lower_response:
+            response = response.lower()
+
+        if response in confirm:
+            await ctx.reply("Proceeding.")
+            return True
+        elif response in cancel:
+            await ctx.reply("Cancelled.")
+            return False
+        else:
+            await ctx.reply("Unknown response, cancelled.")
+            return False
 
     
     
@@ -207,14 +255,41 @@ class Admin_cog(u_custom.CustomCog, name="Admin"):
     
     
     @admin.command(
+        name = "shutdown",
+        aliases = ["shutdown_bot"],
+        brief = "Shuts the bot down.",
+        description = "Shuts the bot down.\nPlease only use if it is the last option."
+    )
+    @commands.check(u_checks.shutdown_check)
+    async def admin_shutdown(self, ctx):
+        ctx = u_custom.CustomContext(ctx)
+
+        letters = list("abcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+        security_code = "".join(random.choice(letters) for i in range(16))
+
+        security = await self._await_confirmation(ctx, message = f"If you're sure you want to do this, send the following string:\n`{security_code}`", confirm = [security_code], cancel = [], lower_response = False)
+
+        if security:
+            # o7
+            await ctx.send("Shutting down.\n<@658290426435862619>")
+
+            await self.bot.close()
+            return
+
+    
+    
+    
+    
+    
+    
+    
+    @admin.command(
         name="load",
         brief = "Load a cog.",
-        description = "Load a cog.",
-        pass_context=True,
-        invoke_without_command=True
+        description = "Load a cog."
     )
     @commands.is_owner()
-    async def load_cog(self, ctx, extension_name: typing.Optional[str]):
+    async def admin_load(self, ctx, extension_name: typing.Optional[str]):
         ctx = u_custom.CustomContext(ctx)
         
         if extension_name is None:
@@ -241,12 +316,10 @@ class Admin_cog(u_custom.CustomCog, name="Admin"):
     @admin.command(
         name="unload",
         brief = "Unload a cog.",
-        description = "Unload a cog.",
-        pass_context=True,
-        invoke_without_command=True
+        description = "Unload a cog."
     )
     @commands.is_owner()
-    async def unload_cog(self, ctx, extension_name: typing.Optional[str]):
+    async def admin_unload(self, ctx, extension_name: typing.Optional[str]):
         ctx = u_custom.CustomContext(ctx)
         
         if extension_name is None:
@@ -273,12 +346,10 @@ class Admin_cog(u_custom.CustomCog, name="Admin"):
     @admin.command(
         name="reload",
         brief = "Reload a cog/module.",
-        description = "Reload a cog/module, note that to load a module like utility.bingo, you need to say 'utility.bingo'.",
-        pass_context=True,
-        invoke_without_command=True
+        description = "Reload a cog/module, note that to load a module like utility.bingo, you need to say 'utility.bingo'."
     )
     @commands.is_owner()
-    async def reload_cog(self, ctx, extension_name: typing.Optional[str]):
+    async def admin_reload(self, ctx, extension_name: typing.Optional[str]):
         ctx = u_custom.CustomContext(ctx)
         
         if extension_name is None:
--- utility/checks.py
+++ utility/checks.py
@@ -1,5 +1,7 @@
 """Along with functions for permission checking, this has some checks that can be used with `@commands.check(checks.<function>)` if this file is loaded as checks via `import utility.checks as checks`."""
 
+from os.path import sep as SLASH
+
 import discord
 from discord.ext import commands
 import typing
@@ -11,22 +13,10 @@ def get_permission(user_id: int, permission_name: str) -> bool:
     """Checks a permission via permissions.json."""
 
     # Load permission data.
-    permission_data = u_files.load("data/permissions.json")
+    permission_data = u_files.load(f"data{SLASH}permissions.json")
 
     # Return a bool for if the user id is in the permissions.
-    return user_id in permission_data[permission_name]
-
-async def bingo_tick_check(ctx: typing.Union[commands.Context, u_custom.CustomContext]) -> bool:
-    """Checks if the user has the permission required to tick and untick tiles on the bingo boards."""
-    
-    if get_permission(ctx.author.id, "bingo_tick"):
-        return True
-
-    # Don't send the message if the check was invoked via the help command.
-    if ctx.invoked_with != "help":
-        await ctx.reply("I am sorry, but you can't use this command.")
-        
-    return False
+    return user_id in permission_data.get(permission_name, [])
 
 def has_role(member: discord.Member, role_list: typing.Union[list[str], list[int]]):
     """Checks if the member provided has any role with an id in the role_list list.
@@ -40,6 +30,9 @@ def has_role(member: discord.Member, role_list: typing.Union[list[str], list[int
     
     return False
 
+
+##### COMMAND CHECKS #####
+
 async def in_authority(ctx: typing.Union[commands.Context, u_custom.CustomContext, discord.Member]) -> bool:
     """Returns a boolean for whether the author of the message (or a member provided) has Trusted or higher.
     This can be used as a check in a command with `@commands.check(checks.in_authority)` or as a standalone check in an if with `if await checks.in_authority(ctx):`"""
@@ -50,3 +43,18 @@ async def in_authority(ctx: typing.Union[commands.Context, u_custom.CustomContex
         member = ctx
 
     return has_role(member, [970549665055522850, 1119445209923723396, 958512048306815056, 958755031820161025, 1179943096402837535])
+
+async def bingo_tick_check(ctx: typing.Union[commands.Context, u_custom.CustomContext]) -> bool:
+    """Checks if the user has the permission required to tick and untick tiles on the bingo boards."""
+    
+    if get_permission(ctx.author.id, "bingo_tick"):
+        return True
+
+    # Don't send the message if the check was invoked via the help command.
+    if ctx.invoked_with != "help":
+        await ctx.reply("I am sorry, but you can't use this command.")
+        
+    return False
+
+async def shutdown_check(ctx: typing.Union[commands.Context, u_custom.CustomContext]) -> bool:
+    return get_permission(ctx.author.id, "shutdown")
