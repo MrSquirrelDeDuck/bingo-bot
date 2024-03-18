ae68388642039a4befe19cb1f3cd08bb432c8343
MrSquirrelDeDuck <72826048+MrSquirrelDeDuck@users.noreply.github.com>
2024-03-18 17:12:57 -0400

    Added `percentage` alias for `%bread percent`.

--- bread_cog.py
+++ bread_cog.py
@@ -1134,6 +1134,7 @@ class Bread_cog(
     
     @bread.command(
         name = "percent",
+        aliases = ["percentage"],
         brief = "Calculates percentages of values.",
         description = "Calculates percentages of values.\nAccepted percentage formats are 0.## or ##%, with ## being the number, of course."
     )
