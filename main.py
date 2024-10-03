37093b24c1a102aadd8dca136bfceb98b5ec65f9
MrSquirrelDeDuck <72826048+MrSquirrelDeDuck@users.noreply.github.com>
2024-10-03 01:07:29 -0400

    Add 30 second timeout to the solver.

--- main.py
+++ main.py
@@ -8,57 +8,61 @@ import os
 import utility.custom as u_custom
 import utility.files as u_files
 
-dotenv.load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-COMMAND_PREFIX = os.getenv('COMMAND_PREFIX')
-OWNER_ID = int(os.getenv('OWNER_ID'))
+def run_bot():
+    dotenv.load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    COMMAND_PREFIX = os.getenv('COMMAND_PREFIX')
+    OWNER_ID = int(os.getenv('OWNER_ID'))
 
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
-intents.presences = True
-intents.reactions = True
-
-
-bot = u_custom.CustomBot(
-    command_prefix = COMMAND_PREFIX,
-    intents = intents, 
-    owner_id = OWNER_ID,
-    help_command = u_custom.CustomHelpCommand()
-)
-
-@bot.event
-async def on_ready():
-    print("on_ready() called.")
-
-    # Save the database, this is assuming on_ready() was called twice.
-    try:
-        bot.database.save_database(make_backup=True)
-        print("Database saved.")
-    except AttributeError:
-        print("Setting up database.")
-        bot.database = u_files.DatabaseInterface()
-        print("Database setup complete.")
+    intents = discord.Intents.default()
+    intents.message_content = True
+    intents.members = True
+    intents.presences = True
+    intents.reactions = True
 
 
-    print("Loading admin_cog.")
+    bot = u_custom.CustomBot(
+        command_prefix = COMMAND_PREFIX,
+        intents = intents, 
+        owner_id = OWNER_ID,
+        help_command = u_custom.CustomHelpCommand()
+    )
 
-    try:
-        await bot.load_extension("admin_cog")
+    @bot.event
+    async def on_ready():
+        print("on_ready() called.")
 
-        admin_cog = bot.get_cog("Admin")
-
-        await admin_cog._load_all_extensions()
-        admin_cog.add_checks()
-    except commands.ExtensionAlreadyLoaded:
-        pass
-    except:
-        raise
-
-    print("admin_cog loaded.")
-
-    print("Fully logged in as {}!".format(bot.user))
-    print("\n##################################\n")
+        # Save the database, this is assuming on_ready() was called twice.
+        try:
+            bot.database.save_database(make_backup=True)
+            print("Database saved.")
+        except AttributeError:
+            print("Setting up database.")
+            bot.database = u_files.DatabaseInterface()
+            print("Database setup complete.")
 
 
-bot.run(TOKEN)
+        print("Loading admin_cog.")
+
+        try:
+            await bot.load_extension("admin_cog")
+
+            admin_cog = bot.get_cog("Admin")
+
+            await admin_cog._load_all_extensions()
+            admin_cog.add_checks()
+        except commands.ExtensionAlreadyLoaded:
+            pass
+        except:
+            raise
+
+        print("admin_cog loaded.")
+
+        print("Fully logged in as {}!".format(bot.user))
+        print("\n##################################\n")
+
+
+    bot.run(TOKEN)
+
+if __name__ == "__main__":
+    run_bot()
